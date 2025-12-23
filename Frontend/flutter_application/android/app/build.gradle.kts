c7e8aa1016eeed7fca4b040c230cb2c33d6d0442
Omar-Lash1n <omaramrabdelkaderlashin@gmail.com>
2025-12-23 22:12:36 +0200

    pushing the name outside the application and downloading a new version in gradle

--- Frontend/flutter_application/android/app/build.gradle.kts
+++ Frontend/flutter_application/android/app/build.gradle.kts
@@ -7,8 +7,17 @@ plugins {
 
 android {
     namespace = "com.example.flutter_application"
-    compileSdk = flutter.compileSdkVersion
+
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
+
+    defaultConfig {
+        applicationId = "com.example.flutter_application"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -19,25 +28,13 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_application"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
+
 
 flutter {
     source = "../.."
