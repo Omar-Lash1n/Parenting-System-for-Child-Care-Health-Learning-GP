d55236a054505a37173038d2498f215f2d0067e6
Youssef Mohamed <youssef@gmail.com>
2026-05-25 00:06:17 +0300

    save current changes

--- Frontend/flutter_application/lib/main.dart
+++ Frontend/flutter_application/lib/main.dart
@@ -62,10 +62,6 @@ import 'package:Ajial/prizes/prize_store_provider.dart';
 import 'package:Ajial/prizes/prize_store_page.dart';
 import 'package:Ajial/specialist-app/application-tracking/providers/specialist_application_provider.dart';
 import 'package:Ajial/specialist-app/application-tracking/screens/specialist_application_tracking_page.dart';
-import 'package:Ajial/lessons/providers/lesson_provider.dart';
-import 'package:Ajial/lessons/screens/lessons_list_screen.dart';
-import 'package:Ajial/lessons/screens/lesson_detail_screen.dart';
-import 'package:Ajial/lessons/screens/lesson_quiz_screen.dart';
 
 // Background handler is only supported on mobile (Android/iOS), not on web.
 @pragma('vm:entry-point')
@@ -207,7 +203,6 @@ void main() async {
         ChangeNotifierProvider(create: (_) => HealthUnitProvider()),
         ChangeNotifierProvider(create: (_) => PrizeStoreProvider()),
         ChangeNotifierProvider(create: (_) => SpecialistApplicationProvider()),
-        ChangeNotifierProvider(create: (_) => LessonProvider()),
       ],
       child: const AjialApp(),
     ),
@@ -270,9 +265,6 @@ class AjialApp extends StatelessWidget {
         '/prizes': (context) => const PrizeStorePage(),
         '/specialist-application': (context) =>
             const SpecialistApplicationTrackingPage(),
-        '/lessons': (context) => const LessonsListScreen(),
-        '/lesson-detail': (context) => const LessonDetailScreen(),
-        '/lesson-quiz': (context) => const LessonQuizScreen(),
       },
     );
   }
