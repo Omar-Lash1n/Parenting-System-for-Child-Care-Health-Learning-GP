5bd381f63a7f3d184130b63d8e4ab562be933076
eslamnaaser454 <nassereslam454@gmail.com>
2026-06-20 00:39:49 +0300

    Medical examination and medical prescription get endpoints

--- Backend/Ajial.Infrastructure/obj/Debug/net9.0/Ajial.Infrastructure.AssemblyInfo.cs
+++ Backend/Ajial.Infrastructure/obj/Debug/net9.0/Ajial.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ajial.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b55a77815f00f879efc77c14cbfab393b916ac88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d09f011f6ec92b85fdedc3ea391ac4f3d25803")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ajial.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ajial.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
